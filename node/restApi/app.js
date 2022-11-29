d6c72d898eaf96616f6ff39d26650a20aa104171
Kavya-N-git <nkavya2017@gmail.com>
2022-11-29 11:31:31 +0530

    done

--- node/restApi/app.js
+++ node/restApi/app.js
@@ -2,17 +2,18 @@ const mongoose = require('mongoose');
 const morgan=require('morgan');
 const express = require('express');
 
+
 let app = express();
 
-//const LiveUrl='mongodb+srv://test:test123@cluster0.pmiyo3t.mongodb.net/testdata?retryWrites=true&w=majority'
+const LiveUrl='mongodb+srv://test:test123@cluster0.pmiyo3t.mongodb.net/testdata?retryWrites=true&w=majority'
 
-
-let dotenv = require('dotenv')
-dotenv.config()
-let port = process.env.PORT || 9800;
-let mongo = require('mongodb');
-let MongoClient = mongo.MongoClient;
-let MongoUrl =process.env.MongoURL;
+//for localDB
+//let dotenv = require('dotenv')
+//dotenv.config()
+//let port = process.env.PORT || 9800;
+//let mongo = require('mongodb');
+//let MongoClient = mongo.MongoClient;
+//let MongoUrl =process.env.MongoURL;
 //let MongoUrl =process.env.LiveMongo;*/
 let db;
 
@@ -51,7 +52,7 @@ app.get('/products',(req,res)=>{
     })
 })*/
 
-/*db connection with compass
+//db connection with atlas
 mongoose.connect(LiveUrl,{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -59,14 +60,14 @@ mongoose.connect(LiveUrl,{
     console.log("connected");
 }).catch((err)=>console.log(err));
 app.listen(9800,()=>{
-    console.log("app running ")
-})*/
+    console.log("app running at port 9800")
+})
 
 //db connection with localDb
-MongoClient.connect(MongoUrl, (err, client)=>{
+/*MongoClient.connect(MongoUrl, (err, client)=>{
     if (err) console.log('error while connection');
     db=client.db('testdata');
     app.listen(port,()=>{
       console.log(`server ${port}`)
   })
-  })
+  })*/
